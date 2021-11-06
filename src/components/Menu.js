9c70af06c40f048cf86ce42dd15e970fe3ee49b5
jakuubs <jakuubs11@gmail.com>
2021-11-06 15:15:11 +0100

    add pizzas to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import Loader from 'react-loader-spinner';
 
 const Menu = () => {
+    
+    const [pizzas, setPizzas] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                await fetch("http://localhost:3333/api/pizza")
+                .then(response => response.json())
+                .then(data => setPizzas(data))
+            } catch (error) {
+                setHasError(true);
+            }
+            setIsLoading(false);
+        }
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <p>Menu</p>
+            <ul>
+                { hasError && <p>Something went wrong!</p> }
+                { isLoading ? (
+                    <Loader
+                    type="Circles"
+                    color="#FFFFFF"
+                    height={100}
+                    width={100} />) : pizzas.map(pizza => <li key={pizza.id}>{pizza.name}</li>) }
+            </ul>
         </div>
     )
 }
