65365640ca8fc9dcbdbe19133f95e57c278c1850
jakuubs <jakuubs11@gmail.com>
2021-11-22 22:11:39 +0100

    fix overflow in checkout page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import pizza from "../images/pizza_planet.png";
 import "../styles/Home.css";
 
 const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="home">
       <div className="main-logo-item">
